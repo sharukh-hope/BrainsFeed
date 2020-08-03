17de2b644de90e2a3318a44132a65938bc66b930
Sharukh Babu <sharukh.hope@gmail.com>
2020-08-03 21:20:08 +0530

    image expansion and some spacing adjustment in frame

--- src/Components/CompanyFrame.js
+++ src/Components/CompanyFrame.js
@@ -3,11 +3,17 @@ import FrameHeader from './FrameHeader';
 import { Button } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import StarRateIcon from '@material-ui/icons/StarRate';
+import { Modal } from 'react-bootstrap';
 
 
 const CompanyFrame = ({ details, changeCurentCompany }) => {
     const [show, setShow] = useState(false);
     const [showtext, setShowtext] = useState("show more...");
+    const [showimg, setShowimg] = useState(false);
+
+    const handleClose = () => {
+        setShowimg(false);
+    };
     const handleMore = () => {
         setShow(true);
         setShowtext("");
@@ -21,56 +27,63 @@ const CompanyFrame = ({ details, changeCurentCompany }) => {
         <div className='frame'>
             <FrameHeader props={details} />
             <div className='frameContent'>
-                <div style={{ display: 'flex', flexDirection: 'column', width: "550px" }}>
+                <div style={{ display: 'flex', flexDirection: 'column', width: "550px", justifyContent: "space-between" }}>
                     {' '}
                     <p>{details.Short_description}</p>
-                    <div className='importantInfo'>
-                        <table>
-                            <tr>
-                                <th>Industry</th>
-                                <th>Rating</th>
-                                <th className="tDivider">Access</th>
-                                <th>API</th>
-                            </tr>
-                            <tr>
-                                <td >{details.industry}</td>
-                                <td style={{ fontSize: "20px" }}> {details.rating}<StarRateIcon /></td>
-                                <td className="tDivider"> {details.paid_not}</td>
-                                <td> {details.API_Provided}</td>
-                            </tr>
-                        </table>
-                        <div>
-                            {
-                                details.specialties.map((ele, index) => {
 
-                                    if (!show)
-                                        while (index < 3)
-                                            return (<>
-                                                <Badge style={{ margin: "2px" }} pill variant="light">
-                                                    {ele}
-                                                </Badge> </>);
-                                    else
-                                        return (<Badge style={{ margin: "2px" }} pill variant="light">
-                                            {ele}
-                                        </Badge>);
+                    <table>
+                        <tr>
+                            <th>Industry</th>
+                            <th>Rating</th>
+                            <th className="tDivider">Access</th>
+                            <th>API</th>
+                        </tr>
+                        <tr>
+                            <td >{details.industry}</td>
+                            <td style={{ fontSize: "20px" }}> {details.rating}<StarRateIcon /></td>
+                            <td className="tDivider"> {details.paid_not}</td>
+                            <td> {details.API_Provided}</td>
+                        </tr>
+                    </table>
+                    <div>
+                        {
+                            details.specialties.map((ele, index) => {
 
-                                })
+                                if (!show)
+                                    while (index < 3)
+                                        return (<>
+                                            <Badge style={{ margin: "2px" }} pill variant="light">
+                                                {ele}
+                                            </Badge> </>);
+                                else
+                                    return (<Badge style={{ margin: "2px" }} pill variant="light">
+                                        {ele}
+                                    </Badge>);
 
-                            } {!show ? (< text className="showText" onClick={handleMore}>{showtext}</text>) : (
-                                <text className="showText" onClick={handleLess}>{showtext}</text>
-                            )}
-                        </div>
-                        <div style={{ width: "100%" }}>
-                            <Button
-                                onClick={() => changeCurentCompany(details)}
-                                style={{ marginTop: 'auto', width: "100%" }}
-                            >
-                                Learn More
-                        </Button>
-                        </div>
+                            })
+
+                        } {!show ? (< text className="showText" onClick={handleMore}>{showtext}</text>) : (
+                            <text className="showText" onClick={handleLess}>{showtext}</text>
+                        )}
                     </div>
+                    <div style={{ width: "100%" }}>
+                        <Button
+                            onClick={() => changeCurentCompany(details)}
+                            style={{ width: "100%" }}
+                        >
+                            Learn More
+                        </Button>
+                    </div>
+
                 </div>
-                <img className='screenShot' src={details.screenshot} />
+                <img className='screenShot' onClick={() => setShowimg(true)} src={details.screenshot} />
+                <Modal size="xl" centered show={showimg} onHide={handleClose}>
+                    <Modal.Header closeButton />
+                    <Modal.Body>
+                        <img className='screenShotLarge' src={details.screenshot} />
+                    </Modal.Body>
+
+                </Modal>
             </div>
         </div >
     );
